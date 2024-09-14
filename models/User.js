441a73e856e8347559f1b7a92751c12fd14699d9
Md Repon <repon7253@gmail.com>
2024-09-14 13:28:12 +0600

    ready user schema

---
+++ models/User.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+// create Schema
+const UserSchema = new mongoose.Schema({
+  userName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    uniqe: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  rele: {
+    type: String,
+    default: "user",
+  },
+});
+
+// create model
+const User = mongoose.model("User", UserSchema);
+
+module.exports = User;
+
