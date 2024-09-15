2ddb41ccb9d091263ba2e63d9678294aff721bee
Md Repon <repon7253@gmail.com>
2024-09-15 21:45:52 +0600

    Check user axists or not

--- controllers/auth/auth-controller.js
+++ controllers/auth/auth-controller.js
@@ -7,6 +7,10 @@ const registerUser = async (req, res) => {
   const { userName, email, password } = req.body;
 
   try {
+
+    const checkUser = await User.findOne({email})
+    if(checkUser) return res.json({success: false, message: "User Aleready exists with the same email! Please try again"})
+
     const hashPassword = await bcrypt.hash(password, 12);
     const newUser = new User({
       userName,
