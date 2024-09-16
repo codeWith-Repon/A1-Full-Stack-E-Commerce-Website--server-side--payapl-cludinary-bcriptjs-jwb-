ac1b7925172368f0d322c9c0d1d9c5e4effc227e
Md Repon <repon7253@gmail.com>
2024-09-16 06:16:44 +0600

    find data in database if user is available then user can allowed

--- controllers/auth/auth-controller.js
+++ controllers/auth/auth-controller.js
@@ -1,5 +1,5 @@
 const bcrypt = require("bcryptjs");
-const jwr = require("jsonwebtoken");
+const jwt = require("jsonwebtoken");
 const User = require("../../models/User");
 
 //register
@@ -31,8 +31,42 @@ const registerUser = async (req, res) => {
 
 //login
 
-const login = async (req, res) => {
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  
   try {
+    //check user register or not 
+    const checkUser = await User.findOne({email})
+
+    if(!checkUser)
+      return res.json({
+      success: false,
+      message: "User dosen't exists! Please register first"
+    })
+
+    //check password 
+    const checkPasswordMatch = await bcrypt.compare(password, checkUser.password);
+    if(!checkPasswordMatch)
+    return res.json({
+      success: false,
+      message: "Incorrect password! Please try again"
+    })
+
+    //create token
+    const token = jwt.sign({
+      id: checkUser._id, role: checkUser.role, email: checkUser.email
+    }, 'CLIENT_SECRET_KEY', {expiresIn: "60m"})
+
+    res.cookie("token",token, {httpOnly: true, secure: false}).json({
+      success: true,
+      message: "Logged in successfully",
+      user: {
+        email: checkUser.email,
+        role: checkUser.role,
+        id: checkUser._id
+      }
+    })
+
   } catch (error) {
     console.log(error),
       res.status(500).json({
@@ -46,4 +80,4 @@ const login = async (req, res) => {
 
 //auth middleware
 
-module.exports = { registerUser };
+module.exports = { registerUser, loginUser };
