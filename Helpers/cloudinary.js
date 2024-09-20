00a3089a972b62b46f3af28fa645838bcb427b77
Md Repon <repon7253@gmail.com>
2024-09-20 09:30:26 +0600

    uplod image function

--- Helpers/cloudinary.js
+++ Helpers/cloudinary.js
@@ -6,3 +6,15 @@ cloudinary.config({
   api_key: "853738973769598",
   api_secret: "Qz65z9fTH_7h0k7lk0MOSI4Sp8o",
 });
+const storage = new multer.memoryStorage();
+
+async function handleImageUpload(file) {
+  const result = await cloudinary.uploader.upload(file, {
+    resource_type: "auto",
+  });
+  return result;
+}
+
+const upload = multer({ storage });
+
+module.exports = { upload, handleImageUpload };
