56f4153fed1a5b8c2640d5179365c124a6c311b0
Md Repon <repon7253@gmail.com>
2024-09-20 09:12:34 +0600

    (come to client) at first create a cloudinary acccount and config in server

---
+++ Helpers/cloudinary.js
@@ -0,0 +1,8 @@
+const cloudinary = require("cloudinary").v2;
+const multer = require("multer");
+
+cloudinary.config({
+  cloud_name: "dccypxuz0",
+  api_key: "853738973769598",
+  api_secret: "Qz65z9fTH_7h0k7lk0MOSI4Sp8o",
+});
