203f65d53863a25e56c9d1b2de6545278ab566d6
Md Repon <repon7253@gmail.com>
2024-09-27 11:10:24 +0600

    F1st ((productController > Product-router)) for product details page

--- controllers/shop/product-controller.js
+++ controllers/shop/product-controller.js
@@ -52,4 +52,28 @@ const getFiltteredProducts = async (req, res) => {
   }
 };
 
-module.exports = { getFiltteredProducts };
+const getProductDetails = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+
+    if (!product)
+      return res.status(404).json({
+        success: false,
+        message: "Product not found!",
+      });
+
+    res.status(200).json({
+      success: true,
+      data: product,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Some error occured",
+    });
+  }
+};
+
+module.exports = { getFiltteredProducts, getProductDetails };
--- routes/shop/products-routes.js
+++ routes/shop/products-routes.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const {
   getFiltteredProducts,
+  getProductDetails,
 } = require("../../controllers/shop/product-controller");
 
 const router = express.Router();
 
 router.get("/get", getFiltteredProducts);
+router.get("/get/:id", getProductDetails)
 
 module.exports = router;
