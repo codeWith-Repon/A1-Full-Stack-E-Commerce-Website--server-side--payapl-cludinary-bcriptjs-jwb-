4893a5045f0703d8f5cede93fc606781b9ee8632
Md Repon <repon7253@gmail.com>
2024-10-08 08:13:09 +0600

    AB2 create orderCapture function

--- controllers/shop/order-controller.js
+++ controllers/shop/order-controller.js
@@ -1,5 +1,6 @@
 const paypal = require("../../Helpers/PayPal");
 const Order = require("../../models/Order");
+const Cart = require("../../models/Cart");
 
 const createOrder = async (req, res) => {
   try {
@@ -15,7 +16,7 @@ const createOrder = async (req, res) => {
       orderUpdateDate,
       paymentId,
       payerId,
-      cartId
+      cartId,
     } = req.body;
 
     const create_payment_json = {
@@ -94,6 +95,33 @@ const createOrder = async (req, res) => {
 
 const capturePayment = async (req, res) => {
   try {
+    const { paymentId, payerId, orderId } = req.body;
+
+    const order = await Order.findById(orderId);
+
+    if (!order) {
+      return res.status(404).json({
+        success: false,
+        message: "Order can not be found",
+      });
+    }
+
+    order.paymentStatus = "paid";
+    order.orderStatus = "confirmed";
+    order.paymentId = paymentId;
+    order.payerId = payerId;
+
+    const getCartId = order.cartId;
+    await Cart.findByIdAndDelete(getCartId)
+
+    await order.save();
+
+    res.status(200).json({
+      success: true, 
+      message: "Order Confirmed",
+      data: order
+    })
+
   } catch (error) {
     console.log(error);
     res.status(500).json({
