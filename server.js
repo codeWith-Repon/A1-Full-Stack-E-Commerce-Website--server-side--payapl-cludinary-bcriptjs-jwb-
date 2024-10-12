adf0cb97efb880cba8a7f315465de45badb3da3f
Md Repon <repon7253@gmail.com>
2024-10-12 14:28:56 +0600

    build search functionality (search-controller > search router > server)

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const shopProductsRouter = require("./routes/shop/products-routes")
 const shopCartRouter = require("./routes/shop/cart-routes")
 const shopAddressRouter = require("./routes/shop/address-routes")
 const shopOrderRouter = require("./routes/shop/order-routes")
+const shopSearchRouter = require("./routes/shop/search-routes")
 
 
 //connect to mongodb
@@ -50,6 +51,7 @@ app.use("/api/shop/products",shopProductsRouter)
 app.use("/api/shop/cart",shopCartRouter)
 app.use("/api/shop/address",shopAddressRouter)
 app.use("/api/shop/order",shopOrderRouter)
+app.use("/api/shop/search", shopSearchRouter)
 
 app.listen(PORT, () =>
   console.log(`server is now running on http://localhost:${PORT}`)
